bfef2c01699abd4b460eda7c7e9c02d3b622e7fc
Regan Ya <hens.virtuella.varld@gmail.com>
2024-06-10 14:32:37 +0200

    feat: add computed selectedCountry depending on cca3 & searchedResultCountries depending on searchInput; change selectedCountries to displayedCountries

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,26 @@
     <CountrySearcher @search="setSearchInput" />
     <CountrySelector :allCountries="allCountries" @select="setCca3" />
     <div
-      v-if="Object.keys(selectedCountries).length > 0"
+      v-if="Object.keys(searchedResultCountries).length > 0"
       class="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3"
     >
       <!-- <div v-for="country in allCountries"> -->
-      <CountryCard v-for="country in selectedCountries" :country="country" />
+      <CountryCard
+        v-for="country in searchedResultCountries"
+        :country="country"
+      />
       <!-- </div> -->
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import allCountries from '@/assets/data/allCountries.json';
 // const uri = 'https://restcountries.com/v3.1/all';
 // const { data: allCountries } = await useFetch(uri);
 
+const displayedCountries = ref([]);
 const searchInput = ref('');
 const cca3 = ref('');
 
@@ -28,15 +32,17 @@ const setSearchInput = (sI) => {
 const setCca3 = (c) => {
   cca3.value = c;
 };
-  );
-};
 
-const searchCountries = (searchInput) => {
-  selectedCountries.value = allCountries.filter(
+const selectedCountry = computed(() =>
+  allCountries.filter((country) => country.cca3 === cca3.value)
+);
+
+const searchedResultCountries = computed(() => allCountries.filter(
     (country) =>
-      country.name.common.toLowerCase().includes(searchInput) === true
+      country.name.common.toLowerCase().includes(searchInput.value) === true
   );
-};
+);
+
 </script>
 
 <style scoped></style>
