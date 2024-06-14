e7835ca7617642a8cda113382c65777db6a99bf3
Regan Ya <hens.virtuella.varld@gmail.com>
2024-06-14 12:00:49 +0200

    feat: add SelectedCountry component

---
+++ components/SelectedCountry.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-5 flex flex-col justify-center gap-4">
+    <div>
+      <CountryCard :country="country" />
+    </div>
+    <div class="flex justify-center">
+      <button
+        @click="onChange"
+        class="px-3 py-2 border-4 rounded-xl text-md font-medium hover:bg-red-200 active:bg-red-300"
+      >
+        Show all result countries
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import allCountries from '@/assets/data/allCountries.json';
+
+const { country } = defineProps(['country']);
+
+const emit = defineEmits('change');
+
+const onChange = () => {
+  const selectedCountry = null;
+  emit('select, selectCountry');
+};
+</script>
